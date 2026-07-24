13ffd74e12f595ec2d18f829c1513b322062df8f
Khalid-Sifullah-Siam <khalidsiam1754@gmail.com>
2026-07-25 00:28:11 +0600

    Reorder navigation and skills

--- src/app/utils/NavItems.ts
+++ src/app/utils/NavItems.ts
@@ -1,14 +1,14 @@
 export const navItems = [
   { name: "Intro", href: "intro", path: "/intro" },
   { name: "About Me", href: "about", path: "/about" },
+  { name: "Projects", href: "projects", path: "/projects" },
+  { name: "Skills & Tools", href: "skills-tools", path: "/skills-tools" },
+  { name: "Experience", href: "experience", path: "/experience" },
   {
     name: "Educational Qualification",
     href: "educational-qualification",
     path: "/educational-qualification",
   },
-  { name: "Projects", href: "projects", path: "/projects" },
-  { name: "Skills & Tools", href: "skills-tools", path: "/skills-tools" },
-  { name: "Experience", href: "experience", path: "/experience" },
   { name: "Blogs", href: "blogs", path: "/blogs" },
   { name: "Contact", href: "contact", path: "/contact" },
 ];
